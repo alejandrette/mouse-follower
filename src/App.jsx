15706181c887e75ac3e9ea78b2652aae624a58ab
Alejandrette <alejandrette5117@gmail.com>
2024-11-19 00:31:45 +0100

    mouse follower

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,56 @@
-import React, { useState } from 'react'
+import { useState, useEffect } from "react";
 
 function App() {
 
-  return (
-    <>
+  const [enable, setEnable] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0});
 
-    </>
+  // Pointer Move
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event;
+      setPosition({ x: clientX, y: clientY });
+    }
+
+    if (enable) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  },[enable]);
+
+  // Cange body className
+  useEffect(() => {
+    document.body.classList.toggle('no-cursor', enable)
+
+    return () => {
+      document.body.classList.remove('no-cursor')
+    }
+  }, [enable])
+
+  return (
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}>
+
+      </div>
+      <button onClick={() => setEnable(!enable)}>
+        {enable ? 'Desactivar' : 'Activar'} seguimiento puntero
+      </button>
+    </main>
   )
 }
 
